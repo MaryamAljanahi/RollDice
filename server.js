4221d6fdb42e72208334eca3e585c4c1dced06f2
Yoomio <maryam.janahi3@gmail.com>
2025-03-02 20:13:35 +0400

    sixth commit

--- server.js
+++ server.js
@@ -16,15 +16,20 @@ app.use(cors({
 // Serve static files (HTML, CSS, images)
 app.use(express.static(path.join(__dirname)));
 
+// Route for the API tester (index.html)
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Route for the Dice Roller UI (dice-roller.html)
+app.get("/dice-roller", (req, res) => {
+    res.sendFile(path.join(__dirname, "dice-roller.html"));
+});
+
 // API route for rolling dice
 app.get("/roll", (req, res) => {
     const diceValue = Math.floor(Math.random() * 6) + 1;
     res.json({ dice: diceValue, message: `You rolled a ${diceValue}!` });
-});
-
-// Default route to serve index.html
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Start the server with a dynamic port for Azure
